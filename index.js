5f8e7ab9a8b0a5494725dd261cb77fc0a85dc530
bstrauss0316 <149720416+bstrauss0316@users.noreply.github.com>
2023-11-08 00:23:32 -0600

    about me4

---
+++ index.js
@@ -0,0 +1,79 @@
+ let number = 15
+ number = 10
+ console.log(number)
+
+ const name = 'Collin'
+console.log(name)
+var number2 = 0
+//number
+number2 = 10
+let x = 2
+let y = 2.5
+let z = x + y
+let b = 100000
+console.log(2**4)
+//string
+const name2 = `good 
+jdasfjb`
+console.log(name2)
+//concantenation
+let sentence = 'hi'
+let sentence2 = 'bye'
+let sentence3 = sentence+ ' ' +sentence2
+//${} - template literall
+let sentence4 = `hi my name is branden. ${sentence2}`
+console.log(sentence4)
+let o = 200
+let s = `${0}`
+let num = '20,000'
+let num2 = 20
+console.log(num+num2)
+const num3 = 10
+console.log(num3)
+
+//booleans
+
+//truthy - anything w value
+//1
+//'great'
+//'true'
+
+//falsy - undefined. no vaule
+//0
+//''
+//false
+//undefined
+//null
+
+console.log(Boolean(null ))
+
+//template literal
+
+//array
+
+
+const emptyArr = [];
+console.log(emptyArr)
+const lengthOfEmptyArray = emptyArr.length;
+console.log(lengthOfEmptyArray)
+
+
+const name1 = "Branden";
+
+const nameArr = ["Branden", "collin"];
+console.log(name1);
+const lengthOfEmptyArray1 = nameArr.length;
+console.log(lengthOfEmptyArray)
+
+console.log(nameArr[1]);
+
+const hello = `hi my name is ${nameArr[1]}`;
+console.log(hello)
+
+nameArr.push("Cortez");
+nameArr.push("Romeo ")
+console.log(nameArr)
+
+console.log(nameArr.pop())
+
+console.log(nameArr.length-1);
